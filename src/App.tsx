ef96e278325f8f62ebf887510026dc5a1de64ac2
Harry7986 <emarh.harry.code@gmail.com>
2026-02-26 09:52:29 +0100

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import './App.css'
+import { InfiniteScrollDemo } from './components/InfiniteScrollDemo';
 
 function App() {
-
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <InfiniteScrollDemo />;
 }
 
-export default App
+export default App;
